d96da0a40ca9871b85e353f1d1cc6debd0901324
David Wilson and Guy Anderson <david@>
2017-04-25 10:12:36 -0700

    created 3 buttons that changed the background color to the page.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,22 @@ $(document).ready(function() {
     $("#initially-showing").slideToggle();
     $("#initially-hidden").slideToggle();
   });
+
+  $("button#green").click(function() {
+   $("#color-background").removeClass();
+   $("#color-background").addClass("green-background");
+ });
+
+ $("button#yellow").click(function() {
+   $("#color-background").removeClass();
+   $("#color-background").addClass("yellow-background");
+ });
+
+ $("button#red").click(function() {
+   $("#color-background").removeClass();
+   $("#color-background").addClass("red-background");
+ });
+
+
+
 });
